7075666e1225d4e98378100de8a5b45f3928b2af
Dhilorz90 <47889223+Dhilorz90@users.noreply.github.com>
2019-02-22 14:27:39 +0100

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,14 @@
+
+<?php
+
+ $destino="tucorreo@correo.com";
+ $nombre=$_POST["name"];
+ $correo=$_POST["email"];;
+ $mensaje=$_POST["message"];
+ $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location: index.html");
+
+
+
+?>
